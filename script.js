497f0eccba27de28cee50dec1cbb5effa6d929ae
Andreas Eriksson <ae225ak@student.lnu.se>
2024-10-10 11:53:26 +0200

    Fix bugs

--- script.js
+++ script.js
@@ -23,90 +23,83 @@ function divide(a, b) {
 }
 
 function operate(firstNumber, operator, secondNumber) {
-  let result
+	let result
 	switch (operator) {
 		case '+':
-      result = add(firstNumber, secondNumber)
+			result = add(firstNumber, secondNumber)
 			break
 
 		case '-':
-      result = subtract(firstNumber, secondNumber)
+			result = subtract(firstNumber, secondNumber)
 			break
 
 		case '*':
-      result = multiply(firstNumber, secondNumber)
+			result = multiply(firstNumber, secondNumber)
 			break
 		case '/':
-      result = divide(firstNumber, secondNumber)
+			result = divide(firstNumber, secondNumber)
 			break
 	}
-  return result
+	return result
 }
-
 
 // Display
 const accumulatedDisplay = document.querySelector('#accumulatedDisplay')
 const currentInputDisplay = document.querySelector('#currentInputDisplay')
 
-
-
-
 // Buttons and logic
 const buttons = document.querySelectorAll('button')
 buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    if (button.classList.contains('number')) {      
-      currentInputDisplay.textContent += button.textContent
-      accumulatedDisplay.textContent += button.textContent
+	button.addEventListener('click', () => {
+		if (button.classList.contains('number')) {
+			currentInputDisplay.textContent += button.textContent
+			accumulatedDisplay.textContent += button.textContent
+		} else if (button.classList.contains('operator') && !operator) {
+			if (!firstNumber) {
+				firstNumber = Number(currentInputDisplay.textContent)
+			} else {
+				secondNumber = Number(currentInputDisplay.textContent)
+			}
+			operator = button.textContent
 
-    } else if (button.classList.contains('operator') && !operator) {
+			currentInputDisplay.textContent = ''
+			accumulatedDisplay.textContent += ` ${operator} `
+		} else if (button.classList.contains('operator') && operator) {
+			secondNumber = Number(currentInputDisplay.textContent)
+			accumulatedDisplay.textContent = operate(
+				firstNumber,
+				operator,
+				secondNumber
+			)
+			firstNumber = operate(firstNumber, operator, secondNumber)
+			currentInputDisplay.textContent = ''
+			operator = button.textContent
+			accumulatedDisplay.textContent += ` ${operator} `
+			operator = button.textContent
+			currentInputDisplay.textContent = ''
+		} else if (button.classList.contains('equals')) {
+			secondNumber = Number(currentInputDisplay.textContent)
+			currentInputDisplay.textContent = ''
 
-      if (!firstNumber) {      
-        firstNumber = Number(currentInputDisplay.textContent)
-      } else {
-        secondNumber = Number(currentInputDisplay.textContent)
-      }
-      operator = button.textContent
+			if (firstNumber && operator === '/' && secondNumber === 0) {
+				accumulatedDisplay.textContent = 'NEJ'
+			} else {
+				// Round answer if not an integer
+				const result = operate(firstNumber, operator, secondNumber)
+				accumulatedDisplay.textContent = Number.isInteger(result)
+					? result
+					: result.toFixed(10)
 
-      currentInputDisplay.textContent = ''      
-      accumulatedDisplay.textContent += ` ${operator} `    
-
-    } else if (button.classList.contains('operator') && operator) {
-      secondNumber = Number(currentInputDisplay.textContent)
-      accumulatedDisplay.textContent = operate(firstNumber, operator, secondNumber)
-      firstNumber = operate(firstNumber, operator, secondNumber)
-      currentInputDisplay.textContent = ''
-      operator = button.textContent
-      accumulatedDisplay.textContent += ` ${operator} ` 
-      operator = button.textContent
-      currentInputDisplay.textContent = ''
-
-    } else if (button.classList.contains('equals')) {
-      secondNumber = Number(currentInputDisplay.textContent)
-      currentInputDisplay.textContent = ''
-
-      // Round answer if not an integer
-      const result = operate(firstNumber, operator, secondNumber)
-      accumulatedDisplay.textContent = Number.isInteger(result) ? result : result.toFixed(10)
-
-
-      firstNumber = operate(firstNumber, operator, secondNumber)
-      secondNumber = ''
-      operator = ''
-      
-    } else if (button.classList.contains('clear')) {
-      currentInputDisplay.textContent = ''
-      accumulatedDisplay.textContent = ''
-      firstNumber = ''
-      secondNumber = ''
-      operator = ''
-    }
-
-  })
-
-
-
-
-
-
+				firstNumber = operate(firstNumber, operator, secondNumber)
+				secondNumber = ''
+				operator = ''
+			}
+		} else if (button.classList.contains('clear')) {
+			currentInputDisplay.textContent = ''
+			accumulatedDisplay.textContent = ''
+			firstNumber = ''
+			secondNumber = ''
+			operator = ''
+		}
+	})
 })
