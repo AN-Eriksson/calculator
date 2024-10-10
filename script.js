b675f78c04209be89a92e48907dc501f33aba6d7
Andreas Eriksson <ae225ak@student.lnu.se>
2024-10-10 09:23:19 +0200

    Add eventListeners to buttons

--- script.js
+++ script.js
@@ -27,22 +27,66 @@ function operate(firstNumber, operator, secondNumber) {
 	switch (operator) {
 		case '+':
       result = add(firstNumber, secondNumber)
-
 			break
 
 		case '-':
       result = subtract(firstNumber, secondNumber)
-
 			break
 
 		case '*':
       result = multiply(firstNumber, secondNumber)
-
 			break
 		case '/':
       result = divide(firstNumber, secondNumber)
-
 			break
 	}
   return result
 }
+
+
+// Display
+const accumulatedDisplay = document.querySelector('#accumulatedDisplay')
+const currentInputDisplay = document.querySelector('#currentInputDisplay')
+
+
+
+
+// Buttons
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (button.classList.contains('number')) {
+      accumulatedDisplay.textContent += button.textContent
+      currentInputDisplay.textContent += button.textContent
+
+    } else if (button.classList.contains('operator')) {
+      firstNumber = Number(currentInputDisplay.textContent)
+      currentInputDisplay.textContent = ''
+      operator = button.textContent
+      accumulatedDisplay.textContent += ` ${operator} `    
+
+    } else if (button.classList.contains('equals')) {
+      secondNumber = Number(currentInputDisplay.textContent)
+      currentInputDisplay.textContent = ''
+
+      accumulatedDisplay.textContent = operate(firstNumber, operator, secondNumber)
+      
+    } else if (button.classList.contains('clear')) {
+      currentInputDisplay.textContent = ''
+      accumulatedDisplay.textContent = ''
+      firstNumber = ''
+      secondNumber = ''
+      operator = ''
+
+    }
+
+
+
+  })
+
+
+
+
+
+
+})
